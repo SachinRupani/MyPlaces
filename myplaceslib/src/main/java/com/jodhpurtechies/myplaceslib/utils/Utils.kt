1318fe8ccaae8f8fcad2e49d0f58b6586b95a93d
sachin <sachin.rupani1992@gmail.com>
2019-09-21 12:41:56 +0530

    Initial Commit

---
+++ myplaceslib/src/main/java/com/jodhpurtechies/myplaceslib/utils/Utils.kt
@@ -0,0 +1,9 @@
+package com.jodhpurtechies.myplaceslib.utils
+
+
+class MyPlaceUtils{
+    val num1 = 10
+    val num2= 20
+
+    fun doAddition():Int = num1+num2
+}
---
+++ myplaceslib/src/main/java/com/jodhpurtechies/myplaceslib/view/PlacesSearchActivity.kt
@@ -0,0 +1,13 @@
+package com.jodhpurtechies.myplaceslib.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jodhpurtechies.myplaceslib.R
+
+class PlacesSearchActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_places_search)
+    }
+}
